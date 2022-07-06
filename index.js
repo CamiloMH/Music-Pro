c4c03a95b0a0630f077fe6eb25dc666aaebceaff
Camilo <camilo.mh2014@gmail.com>
2022-07-06 01:10:44 -0400

    export server

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const app = require('./src/app')
 const sequelize = require('./src/db/db')
-const models = require('./src/models/index')
+// const models = require('./src/models/index')
 const PORT = process.env.PORT || 3000
 
 // Iniciar servidor
-app.listen(PORT, async () => {
+const server = app.listen(PORT, async () => {
   try {
     // FORCE true: DROP TABLE
     await sequelize.sync({ force: false, alter: true })
@@ -14,3 +14,7 @@ app.listen(PORT, async () => {
     console.error('Unable to connect to the database:', error)
   }
 })
+
+module.exports = {
+  server
+}
