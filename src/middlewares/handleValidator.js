67c296b9eac2616a2241e20fc53788a1a8dd6261
Camilo <camilo.mh2014@gmail.com>
2022-07-07 23:26:56 -0400

    ref middleware errors

--- src/middlewares/handleValidator.js
+++ src/middlewares/handleValidator.js
@@ -1,13 +1,19 @@
 const { validationResult } = require('express-validator')
 
 const validateResults = (req, res, next) => {
-  try {
-    validationResult(req).throw()
-    return next()
-  } catch (err) {
-    res.status(403)
-    res.send({ errors: err.array() })
+  const errores = validationResult(req)
+  const err = []
+  for (const error of errores.array()) {
+    err.push(error.msg)
   }
+
+  if (!errores.isEmpty()) {
+    return res.status(400).json({
+      Error: err
+    })
+  }
+
+  next()
 }
 
 module.exports = validateResults
